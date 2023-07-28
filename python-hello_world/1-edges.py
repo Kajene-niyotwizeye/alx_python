132b768d937ecf46656b774eeb44161fe2f8ee80
Kajene-niyotwizeye <niyokajene20@gmail.com>
2023-07-28 06:22:28 -0700

    Python

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -1,13 +1,19 @@
 #!/usr/bin/python3
 word = "Holberton"
-# YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
-word = "Hello"
+
+# Getting the first 3 letters using slicing
 word_first_3 = word[:3]
+
+# Getting the last 2 letters using slicing
 word_last_2 = word[-2:]
-middle_word = word[1:-1]
-print(word_first_3)
-print(word_last_2)
-print(middle_word)
+
+# Getting the middle word using slicing
+middle_start = (len(word) - 2) // 2
+middle_end = middle_start + 2
+middle_word = word[middle_start:middle_end]
+
+# Printing the results
 print("First 3 letters: {}".format(word_first_3))
 print("Last 2 letters: {}".format(word_last_2))
 print("Middle word: {}".format(middle_word))
+
