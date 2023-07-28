865f0be3bcc27a70334e863c1fab83d7f94857d8
Kajene-niyotwizeye <niyokajene20@gmail.com>
2023-07-28 06:56:38 -0700

    Python

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -1,19 +1,8 @@
 #!/usr/bin/python3
+#the words 
+#that comes above
 word = "Holberton"
-
-# Getting the first 3 letters using slicing
-word_first_3 = word[:3]
-
-# Getting the last 2 letters using slicing
-word_last_2 = word[-2:]
-
-# Getting the middle word using slicing
-middle_start = (len(word) - 2) // 2
-middle_end = middle_start + 2
-middle_word = word[middle_start:middle_end]
-
-# Printing the results
+word_first_3, word_last_2, middle_word = word[:3], word[-2:], word[1:-1]
 print("First 3 letters: {}".format(word_first_3))
 print("Last 2 letters: {}".format(word_last_2))
 print("Middle word: {}".format(middle_word))
-
--- python-hello_world/4-print_hexa.py
+++ python-hello_world/4-print_hexa.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-for i in range(99):
-    print(f"{i} (hex: {hex(i)})")
+    for i in range(99):
+    print(f"{i} {hex(i)}")
