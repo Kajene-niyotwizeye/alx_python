ddc9379bb2fa6547689d3230a185546aae1326a6
Kajene-niyotwizeye <niyokajene20@gmail.com>
2023-08-01 02:43:04 -0700

    change

--- python-import_modules/2-variable_load.py
+++ python-import_modules/2-variable_load.py
@@ -1,14 +1,4 @@
-import importlib.util
-
-def import_variable(file_path, variable_name):
-    spec = importlib.util.spec_from_file_location("module_name", file_path)
-    module = importlib.util.module_from_spec(spec)
-    spec.loader.exec_module(module)
-    return getattr(module, variable_name)
-
+#!/usr/bin/python3
 if __name__ == "__main__":
-    file_path = "variable_load_2.py"
-    variable_name = "a"
-    a_value = import_variable(file_path, variable_name)
-    print(a_value)
-
+    from variable_load_5 import a
+    print(a)
