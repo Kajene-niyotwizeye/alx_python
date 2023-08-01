326e9f8044e7c77ff34439e5612f8a7439151444
Kajene-niyotwizeye <niyokajene20@gmail.com>
2023-08-01 04:49:46 -0700

    changes

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-def square_matrix_simple(matrix=[]):
-    return [[y ** 2 for y in x] for x in matrix]
+def square_matrix_map(matrix=[]):
+    return list(map(lambda row: list(map(lambda i: i ** 2, row)), matrix))
