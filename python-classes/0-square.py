66d49913de3924899d868b6414b9e6bc826f6da6
Kajene-niyotwizeye <niyokajene20@gmail.com>
2023-08-01 07:27:17 -0700

    change

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,4 +1,9 @@
-#!/usr/bin/python3
 class Square:
-    """Empty class"""
-    pass
+    def __init__(self, size):
+        self.__size = size
+
+# Example usage:
+square = Square(5)
+# This will raise an AttributeError because __size is private
+print(square.__size)
+
