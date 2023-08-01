f611391ee0ef48545ccfd0d24ad355a47517edde
Kajene-niyotwizeye <niyokajene20@gmail.com>
2023-08-01 02:33:18 -0700

    changes

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -1,18 +1,21 @@
+#!/usr/bin/python3
 import sys
 
-def print_arguments(argv):
-    num_args = len(argv)
+if __name__ != "__main__":
+    exit()
 
-    print(f"Number of argument(s): {num_args}", end="")
-    if num_args == 0:
-        print(".", end="")
-    elif num_args == 1:
-        print(f", followed by: {argv[0]}\n1: {argv[0]}")
-    else:
-        print(":", end="")
-        for i, arg in enumerate(argv):
-            print(f"\n{i + 1}: {arg}")
+argStr = "{:d} argument"
+argc = len(sys.argv) - 1
+if argc == 0:
+    argStr += 's.'
+elif argc == 1:
+    argStr += ':'
+else:
+    argStr += 's:'
+print(argStr.format(argc))
 
-if __name__ == "__main__":
-    args = sys.argv[1:]  # Excluding the script name from arguments
-    print_arguments(args)
+i = 0
+for arg in sys.argv:
+    if i != 0:
+        print("{:d}: {:s}".format(i, arg))
+    i += 1
