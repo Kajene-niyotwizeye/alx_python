fe3f1f7294bc311b19a0c70c4e72b4a7557cc215
Kajene-niyotwizeye <niyokajene20@gmail.com>
2023-08-01 02:17:16 -0700

    Python - import & modules

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,5 +1,11 @@
-# File: add_0.py
+# File: main.py
 
 def add(a, b):
     return a + b
+
+a = 1
+b = 2
+result = add(a, b)
+
+print(f"{a} + {b} = {result}")
 
--- python-import_modules/0-import_add.py
+++ python-import_modules/0-import_add.py
@@ -1,8 +1,1 @@
-
-a = 1
-b = 2
-
-from add_0 import add
-
-result = add(a, b)
-print(f"{a} + {b} = {result}")
+__import__("0-add")
