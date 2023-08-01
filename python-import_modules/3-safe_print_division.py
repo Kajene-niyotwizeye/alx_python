f8d5de5c3bf99bda9a27298557ba75f1f33d9b1e
Kajene-niyotwizeye <niyokajene20@gmail.com>
2023-08-01 08:20:43 -0700

    :changes

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -1,18 +1,9 @@
+#!/usr/bin/python3
 def safe_print_division(a, b):
     try:
-        result = a / b
+        quot = a / b
     except ZeroDivisionError:
-        print("Error: Division by zero is not allowed.")
-        return None
-    except Exception as e:
-        print("Error:", e)
-        return None
+        quot = None
     finally:
-        print("Inside result: {}".format(result) if 'result' in locals() else "Inside result: None")
-        return result
-
-# Test the function with some examples
-safe_print_division(10, 2)
-safe_print_division(5, 0)
-safe_print_division(8, 3)
-
+        print("Inside result: {}".format(quot))
+    return quot
--- python-import_modules/5-raise_exception_msg.py
+++ python-import_modules/5-raise_exception_msg.py
@@ -1,11 +1,3 @@
-def raise_exception_msg(message=""):
-    class NameException(Exception):
-        pass
-
-    raise NameException(message)
-
-# Example usage:
-try:
-    raise_exception_msg("This is a custom exception message.")
-except NameException as e:
-    print("Caught custom exception:", str(e))
+#!/usr/bin/python3
+def raise_exception():
+    raise TypeError
