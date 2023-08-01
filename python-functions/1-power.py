3448f5f65933e9d816214cbb750c9216a03d884a
Kajene-niyotwizeye <niyokajene20@gmail.com>
2023-08-01 07:58:40 -0700

    changes

--- python-functions/1-power.py
+++ python-functions/1-power.py
@@ -1,19 +1,3 @@
+#!/usr/bin/python3
 def pow(a, b):
-    result = 1
-    if b < 0:
-        a = 1 / a
-        b = -b
-
-    while b > 0:
-        if b % 2 == 1:
-            result *= a
-        a *= a
-        b //= 2
-
-    return result
-
-# Test the function
-print(pow(2, 3))  # Output: 8
-print(pow(5, 0))  # Output: 1
-print(pow(3, -2))  # Output: 0.1111111111111111
-
+    return (a ** b)
--- python-functions/3-string.py
+++ python-functions/3-string.py
@@ -1,6 +1,4 @@
-Write a Python function called reverse_string that takes a string as input and returns the reverse of that string.
-
-Prototype: def reverse_string(string)
-Returns the reversed string.
-You are not allowed to import any module.
-You don’t need to understand __import__
+#!/usr/bin/python3
+str = "Holberton School"
+print("{}{}{}".format(str, str, str))
+print(str[0:9])
