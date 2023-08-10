81ec335728d62e2583a7f1c436f66c0529cbb5d1
Kajene-niyotwizeye <niyokajene20@gmail.com>
2023-08-10 07:13:49 -0700

    rwanda

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -1,22 +1,12 @@
 #!/usr/bin/python3
 """
-Displays the value of the X-Request-Id variable
-found in the header of the response
+ fetches https://alx-intranet.hbtn.io/status
 """
+
 import requests
-from sys import argv
 
-
-def main(argv):
-    """
-    Method that takes in a URL, sends a request to the URL
-    and displays the value of the X-Request-Id variable
-    found in the header of the response
-    """
-    url = argv[1]
-    r = requests.get(url)
-    headers = r.headers.get('X-Request-Id')
-    print(headers)
-
-if __name__ == "__main__":
-    main(argv)
+if __name__ == '__main__':
+    r = requests.get('https://alx-intranet.hbtn.io/status')
+    print("Body response:")
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))
