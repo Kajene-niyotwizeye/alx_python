37524bb67e3f63a843a85c1b00d866ca4827c03f
Kajene-niyotwizeye <niyokajene20@gmail.com>
2023-08-10 07:40:48 -0700

    network

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -2,7 +2,7 @@
 """
 POST request to the passed URL with the email as a parameter
 """
-import requests
+import urllib.request
 from sys import argv
 
 
@@ -12,9 +12,13 @@ def main(argv):
     and displays the body of the response (decoded in utf-8)
     """
     values = {'email': argv[2]}
+    data = urllib.parse.urlencode(values)
+    data = data.encode('utf8')
     url = argv[1]
-    r = requests.post(url, data=values)
-    print(r.text)
+    req = urllib.request.Request(url, data)
+    with urllib.request.urlopen(req) as response:
+        result = response.read()
+        print(result.decode('utf8'))
 
 if __name__ == "__main__":
     main(argv)
