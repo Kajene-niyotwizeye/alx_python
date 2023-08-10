c26f0b1548402f01a7972ff6b7e2020a8821203a
Kajene-niyotwizeye <niyokajene20@gmail.com>
2023-08-10 07:29:42 -0700

    POST an email #1

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -1,12 +1,22 @@
 #!/usr/bin/python3
 """
- fetches https://alx-intranet.hbtn.io/status
+Displays the value of the X-Request-Id variable
+found in the header of the response
 """
-
 import requests
+from sys import argv
 
-if __name__ == '__main__':
-    r = requests.get('https://alx-intranet.hbtn.io/status')
-    print("Body response:")
-    print("\t- type: {}".format(type(r.text)))
-    print("\t- content: {}".format(r.text))
+
+def main(argv):
+    """
+    Method that takes in a URL, sends a request to the URL
+    and displays the value of the X-Request-Id variable
+    found in the header of the response
+    """
+    url = argv[1]
+    r = requests.get(url)
+    headers = r.headers.get('X-Request-Id')
+    print(headers)
+
+if __name__ == "__main__":
+    main(argv)
--- python-network_1/4-error_code.py
+++ python-network_1/4-error_code.py
@@ -2,7 +2,7 @@
 """
 Sends a request to the URL and displays the body of the response.
 """
-import urllib.request
+import requests
 from sys import argv
 
 
@@ -12,13 +12,11 @@ def main(argv):
     print: Error code: followed by the HTTP status code
     """
     url = argv[1]
-    req = urllib.request.Request(url)
-    try:
-        with urllib.request.urlopen(req) as response:
-            result = response.read()
-            print(result.decode('utf8'))
-    except urllib.error.URLError as e:
-        print("Error code: {}".format(e.code))
+    r = requests.get(url)
+    if r.status_code == requests.codes.ok:
+        print(r.text)
+    else:
+        print("Error code: {}".format(r.status_code))
 
 
 if __name__ == "__main__":
