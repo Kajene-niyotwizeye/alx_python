3a4bd0f069afe5d371df576f4ad75535c4b321e5
Kajene-niyotwizeye <niyokajene20@gmail.com>
2023-08-10 05:52:11 -0700

    Python inheritance

--- python-inheritance/8-square.py
+++ python-inheritance/8-square.py
@@ -15,3 +15,7 @@ class Square(Rectangle):
         self.integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
+
+    def __str__(self):
+        """Sets the str behaviour."""
+        return "[Square] {:d}/{:d}".format(self.__size, self.__size)
